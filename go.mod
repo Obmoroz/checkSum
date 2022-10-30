56284733625775487b65f599efcf3d7eceec21aa
Obmoroz <Obmorozz@gmail.com>
2022-10-30 20:45:51 +0300

    необходимо решить вопрос с одновременным запуском GC.

--- go.mod
+++ go.mod
@@ -2,19 +2,4 @@ module checkSum
 
 go 1.18
 
-require (
-	github.com/codegangsta/cli v1.20.0 // indirect
-	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/inconshreveable/axiom v0.0.0-20160929160803-68a33d102934 // indirect
-	github.com/inconshreveable/gonative v0.0.0-20160721193423-1c70352219a1 // indirect
-	github.com/inconshreveable/log15 v0.0.0-20201112154412-8562bdadbbac // indirect
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
-	github.com/kr/binarydist v0.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	gopkg.in/inconshreveable/go-update.v0 v0.0.0-20150814200126-d8b0b1d421aa // indirect
-	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require gopkg.in/yaml.v3 v3.0.1
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
 /*
 #cgo LDFLAGS: -ldl
-//#include <dlfcn.h>
-//typedef long long GoInt64;
-//typedef unsigned long long GoUint64;
-//typedef GoInt64 GoInt;
-//typedef GoUint64 GoUint;
-//typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
-static void callFromLib(void* p,_GoString_ HType,_GoString_ FilePath,_GoString_ Mail) {
-    void (*fn)(_GoString_,_GoString_,_GoString_);
-    *(void**)(&fn) =p ;
-    fn(HType,FilePath,Mail);
-}
+//#include <windows.h>
+////#include <dlfcn.h>
+////typedef long long GoInt64;
+////typedef unsigned long long GoUint64;
+////typedef GoInt64 GoInt;
+////typedef GoUint64 GoUint;
+////typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
+//static void callFromLib(void* p,_GoString_ HType,_GoString_ FilePath,_GoString_ Mail) {
+//   void (*fn)(_GoString_,_GoString_,_GoString_);
+//   *(void**)(&fn) =p ;
+//   fn(HType,FilePath,Mail);
+//}
 
 */
 import "C"
-import (
-	"checkSum/config"
-	"fmt"
-	"os"
-	"path"
-	"time"
-)
 
 func main() {
 	readFlags()
@@ -31,25 +32,69 @@ func main() {
 			"для получения справки введите флаг -help")
 		os.Exit(1)
 	}
-	RootDir, err := config.RootDirIdentification()
+	//RootDir, err := config.RootDirIdentification()
+	//if err != nil {
+	//	return
+	//}
+	lib, err := syscall.LoadDLL("lib.dll")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	proc, err := lib.FindProc("MakeHash")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	fmt.Printf("%+v\n", proc)
+
+	fmt.Printf("%s %s %s\n", flagHashType, flagFilePath, flagMail)
+	fmt.Printf("%v %v %v\n", uintptr(unsafe.Pointer(&flagHashType)), uintptr(unsafe.Pointer(&flagFilePath)), uintptr(unsafe.Pointer(&flagMail)))
+
+	call, _, err := proc.Call(uintptr(unsafe.Pointer(&flagHashType)), uintptr(unsafe.Pointer(&flagFilePath)),
+		uintptr(unsafe.Pointer(&flagMail)))
 	if err != nil {
 		return
 	}
 
-	handle := C.dlopen(C.CString(path.Join(RootDir, "lib", "lib.so")), C.RTLD_LAZY)
-	if handle == nil {
-		fmt.Printf("error opening ./lib/lib.h")
-		return
+	if err != nil {
+		fmt.Printf("%+v \r\n", err.Error())
 	}
-	sdPidGetUnit := C.dlsym(handle, C.CString("MakeHash"))
-	if sdPidGetUnit == nil {
-		fmt.Printf("error resolving sd_pid_get_unit function")
-		return
-	}
-	C.callFromLib(sdPidGetUnit, flagHashType, flagFilePath, flagMail)
 
-	time.Sleep(10 * time.Second)
+	fmt.Printf("%+v \n", call)
 
-	fmt.Printf("RunLib is at %p\n", sdPidGetUnit)
+	//ret, _, callErr := syscall.SyscallN(proc,
+	//	3,
+	//	uintptr(unsafe.Pointer(&flagHashType)),
+	//	uintptr(unsafe.Pointer(&flagFilePath)),
+	//	uintptr(unsafe.Pointer(&flagMail)),
+	//)
+	//
+	//if callErr != 0 {
+	//	fmt.Printf("%+v \r\n", callErr.Error())
+	//}
+	//
+	//fmt.Printf("%+v \n", ret)
+
+	//fmt.Printf("%+v", RootDir+"\\lib\\lib.dll")
+	//
+	//handle := C.LoadLibrary(C.CString(RootDir + "\\lib\\lib.dll"))
+	//fmt.Printf("%+v", handle)
+	//if handle == nil {
+	//	fmt.Printf("error opening ./lib/lib.h")
+	//	return
+	//}
+	//fmt.Printf("%+v", handle)
+	//
+	//sdPidGetUnit := C.GetProcAddress(handle, C.CString("MakeHash"))
+	//if sdPidGetUnit == nil {
+	//	fmt.Printf("error resolving sd_pid_get_unit function")
+	//	return
+	//}
+	//C.callFromLib(sdPidGetUnit, flagHashType, flagFilePath, flagMail)
+
+	//	time.Sleep(10 * time.Second)
+
+	//	fmt.Printf("RunLib is at %p\n", sdPidGetUnit)
 
 }
